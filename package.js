be089e1d88d4ac28f2f8fd7d9ebf6a99051d0ec3
Kelly Copley <copleykj@gmail.com>
2024-07-21 12:11:27 -0400

    v0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* global Package */
 Package.describe({
   name: 'typed:model',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'A Zod validated wraper around Meteor\'s Mongo.Collection for your meteor app',
   git: 'https://github.com/copleykj/meteor-typed-model',
   documentation: 'README.md'
